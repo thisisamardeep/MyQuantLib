cd0c5f216031acecf0ca9fdb0d1f65735db68593
amardeep <amardeepjk@gmail.com>
2024-02-25 19:31:41 +0800

    wip

---
+++ Examples/CVAIRS/CVAIRS.cpp
@@ -0,0 +1,15 @@
+
+
+/*
+  This example reproduces Table 2 on page 11 of 
+  A Formula for Interest Rate Swaps Valuation under
+  Counterparty Risk in presence of Netting Agreements
+  
+  Damiano Brigo and Massimo Masetti; May 4, 2005
+ */
+
+int main(int, char* []) {
+
+    int t=67;
+}
+
